253b5e43259452bdb6c182425dc33c4dd0e9d160
Busik <Busik96@gmail.com>
2019-09-19 15:45:58 +0200

    Add user edit page and static page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
+  rescue_from ActionView::MissingTemplate, with: :error_render_method
+
   def index; end
+
+  def static_page
+    render params[:page]
+  end
+
+  private
+
+  def error_render_method
+    raise ActionController::RoutingError.new('Not Found')
+  end
 end
