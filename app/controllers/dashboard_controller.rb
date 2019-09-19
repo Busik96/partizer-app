bd0f2bee28f7cf6971f01371c655e34e14c63e66
Busik <Busik96@gmail.com>
2019-09-19 12:24:27 +0200

    Create dashboard
    Proszę!

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+class DashboardController < ProtectedController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
-  root to: 'home#index'
+
+  authenticated :user do
+    root 'dashboard#index', as: :authenticated_root
+  end
+
+  root "home#index"
 end
