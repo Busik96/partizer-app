6239152530a9dcd5159d922ace8edecb13bbe84e
Busik <Busik96@gmail.com>
2019-09-24 13:06:19 +0200

    Companies view and search field

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,7 +1,20 @@
 # frozen_string_literal: true
 
 class CompaniesController < ApplicationController
+  before_action :preload_data
+
   def index
-    @companies = Company.all
+    @companies = SearchCompanyQuery.new.call(Company.all, search_params)
+  end
+
+  private
+
+  def preload_data
+    @categories = Category.all
+    @search_params = search_params
+  end
+
+  def search_params
+    params.permit(:query, :category_id) || {}
   end
 end
