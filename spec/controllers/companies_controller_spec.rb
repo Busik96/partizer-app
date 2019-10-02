c796648189cdce5b52459e383f53137ea1295e30
Busik <Busik96@gmail.com>
2019-10-02 12:08:13 +0200

    specki dla Mareczka :+1:

--- spec/controllers/companies_controller_spec.rb
+++ spec/controllers/companies_controller_spec.rb
@@ -3,12 +3,13 @@
 require 'rails_helper'
 
 RSpec.describe CompaniesController, type: :controller do
-  describe 'GET companies#index' do
-    let(:category1) { create :category }
-    let(:category2) { create :category }
-    let!(:company1) { create :company, categories: [category1] }
-    let!(:company2) { create :company, categories: [category1, category2] }
+  let(:category1) { create :category }
+  let(:category2) { create :category }
+  let!(:company_page1) { create :company_page, company: company1 }
+  let!(:company1) { create :company, :with_photos, categories: [category1] }
+  let!(:company2) { create :company, categories: [category1, category2] }
 
+  describe 'GET companies#index' do
     context 'without search' do
       before { get :index }
 
@@ -48,6 +49,55 @@ RSpec.describe CompaniesController, type: :controller do
           expect(assigns(:companies)).to eq([company2])
         end
       end
+    end
+  end
+
+  describe 'GET companies#show' do
+    before { get :show, params: { id: company1.id } }
+    it 'redirect to basic view' do
+      expect(response).to redirect_to(basics_company_path)
+    end
+  end
+
+  describe 'GET companies#basics' do
+    before do
+      get :basics, params: { id: company1.id }
+    end
+
+    it 'renders basics view' do
+      expect(response).to render_template('basics')
+    end
+
+    it 'assigns all company photos to @photos' do
+      expect(assigns(:photos).to_a).to eq(company1.photos.to_a)
+    end
+  end
+
+  describe 'GET companies#files' do
+    before do
+      get :files, params: { id: company1.id }
+    end
+
+    it 'renders basics view' do
+      expect(response).to render_template('files')
+    end
+
+    it 'assigns all company files to @files' do
+      expect(assigns(:files).to_a).to eq(company1.company_files.to_a)
+    end
+  end
+
+  describe 'GET companies#show_page' do
+    before do
+      get :show_page, params: { id: company1.id, page: company_page1 }
+    end
+
+    it 'renders show_page view' do
+      expect(response).to render_template('show_page')
+    end
+
+    it 'assigns comapny page to @page' do
+      expect(assigns(:page)).to eq(company_page1)
     end
   end
 end
