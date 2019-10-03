12621f4636c67450a5bb1d0eb76eaac230423cfb
Busik <Busik96@gmail.com>
2019-10-03 14:47:17 +0200

    add association and translate to party_template

--- spec/models/party_template_spec.rb
+++ spec/models/party_template_spec.rb
@@ -7,6 +7,7 @@
 #  id          :bigint           not null, primary key
 #  description :string
 #  name        :string
+#  published   :boolean          default(FALSE)
 #  settings    :jsonb
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
@@ -17,6 +18,7 @@ require 'rails_helper'
 RSpec.describe PartyTemplate, type: :model do
   describe 'associations' do
     it { is_expected.to have_many(:party_template_elements) }
+    it { is_expected.to have_many(:parties) }
   end
 
   describe 'validations' do
