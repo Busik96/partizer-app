dc52b488ac17bb9267c8ac44e969c83154e72b42
Busik <Busik96@gmail.com>
2019-11-05 15:55:18 +0100

    create complete parties controller

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -10,15 +10,28 @@ class PartiesController < ProtectedController
     @party.build_address
   end
 
+  def edit
+    @party_id = params[:id]
+    @current_url = edit_party_path(id: @party_id)
+    @categories = Category.all.map { |cat| { id: cat.id, value: cat.name } }
+  end
+
   def create
     @party = current_user.parties.new(party_params)
     if @party.save
-      flash[:success] = 'Pomyślnie stworzono impreze!'
-      redirect_to parties_path
+      PartyServices::SetupTemplate.new.call(@party)
+      redirect_to edit_party_path(@party)
     else
       flash[:error] = 'Coś poszło nie tak!'
       render 'new'
     end
+  end
+
+  def destroy
+    @party = Party.find(params[:id])
+    @party.destroy
+    flash[:success] = "Poprawnie usuneliśmy impreze! #{@party.name}"
+    redirect_to parties_path
   end
 
   private
@@ -26,7 +39,7 @@ class PartiesController < ProtectedController
   def party_params
     params.require(:party).permit(
       :name, :start_date, :end_date,
-      party_template_id: [],
+      :party_template_id,
       address_attributes: %i[id address1 address2 city zipcode phone_number]
     )
   end
